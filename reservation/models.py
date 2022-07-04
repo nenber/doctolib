d6cc5112f0fea06f711653c3271c7fb377299364
Céline LY <lycelinepro99@gmail.com>
2022-07-04 14:23:20 +0200

    Add Entity Database

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.forms import BooleanField
 
 # Create your models here.
+class Reservation(models.Model):
+    description = models.TextField()
+    # ManyToOne
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor  = models.ForeignKey(User, on_delete=models.CASCADE)
+    # OneToOne
+    slot = models.OneToOneField(
+        TimeSlot,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+class TimeSlot(models.Model): 
+    slotStart = models.DateField()
+    slotEnd = models.DateField()
+    # ManyToOne
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    available = models.BooleanField()
