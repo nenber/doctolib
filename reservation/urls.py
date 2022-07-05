5c15f035d2ab893d3cb1518132e4c5ead212596a
nath026 <nathalie.ton026@gmail.com>
2022-07-05 16:26:05 +0200

    patient go to doctor timeslots

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
- path("create-timeslot/", views.create_timeslot, name="create-timeslot"),
+  path("create-timeslot/", views.create_timeslot, name="create-timeslot"),
   path("list-timeslot/", views.list_timeslot, name="list-timeslot"),
   path("", views.backoffice, name="backoffice"),
   path("list-reservation/", views.list_reservation, name="list-reservation"),
+  path("<int:doctor_id>/timeslot", views.doctor_timeslot, name="doctor-timeslot"),
 ]
