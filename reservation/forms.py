0e0df001638d97fd122de2cb6060de41705386c0
nath026 <nathalie.ton026@gmail.com>
2022-07-05 17:49:52 +0200

    create reservation

--- reservation/forms.py
+++ reservation/forms.py
@@ -3,10 +3,18 @@
 from django import forms
 
 from authentication.models import User
-from .models import TimeSlot
+from .models import Reservation, TimeSlot
  
 class CreateTimeSlot(forms.ModelForm):
     doctor = forms.ModelChoiceField(widget=forms.HiddenInput(), queryset=User.objects.all())
     class Meta:
         model = TimeSlot
         fields = ('slotStart', 'slotEnd', 'doctor', 'available')
+
+class CreateReservation(forms.ModelForm):
+    slot = forms.ModelChoiceField(widget=forms.HiddenInput(), queryset=TimeSlot.objects.all())
+    patient = forms.ModelChoiceField(widget=forms.HiddenInput(), queryset=User.objects.all())
+    doctor = forms.ModelChoiceField(widget=forms.HiddenInput(), queryset=User.objects.all())
+    class Meta:
+        model = Reservation
+        fields = ('patient', 'doctor', 'slot')
