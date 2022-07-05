66861f347b3e2903117c4ff31e7020da62628734
Céline LY <lycelinepro99@gmail.com>
2022-07-05 10:49:01 +0200

    admin

--- reservation/views.py
+++ reservation/views.py
@@ -10,29 +10,35 @@ from .forms import CreateTimeSlot
 from . import forms
 # Create your views here.
 def create_timeslot(request):
+    context ={}
+    if request.user.role != 'DOCTOR':
+        return redirect(settings.LOGIN_REDIRECT_URL)
     # dictionary for initial data with
     # field names as keys
-    context ={}
 
-    # add the dictionary during initialization
+        # add the dictionary during initialization
     form = CreateTimeSlot(request.POST or None)
     if form.is_valid():
         form.save()
 
-    current_doctor = request.user
+        # current_doctor = request.user
     context['form'] = form
-    context['current_doctor'] = current_doctor
+        # context['current_doctor'] = current_doctor
     return render(request, "timeslot/create_timeslot.html", context)
 
 def list_timeslot(request):
+    context ={}
+    if request.user.role != 'DOCTOR':
+        return redirect(settings.LOGIN_REDIRECT_URL)
     # dictionary for initial data with
     # field names as keys
-    context ={}
 
-    # add the dictionary during initialization
+        # add the dictionary during initialization
     context["dataset"] = TimeSlot.objects.all()
 
     return render(request, "timeslot/list_timeslot.html", context)
 
-def backoffice(request):       
+def backoffice(request):
+    if request.user.role != 'DOCTOR':       
+        return redirect(settings.LOGIN_REDIRECT_URL)
     return render(request, "backoffice.html")
