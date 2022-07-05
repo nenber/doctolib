e7b5100b25ca739121253d00289e5a83cdf2c49e
Céline LY <lycelinepro99@gmail.com>
2022-07-05 12:31:38 +0200

    complete profile docteur

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,11 +2,14 @@
 
 from django.urls import path
 
+
 from . import views
 urlpatterns = [
 
  path('', views.home, name = "home"),
  path("signup/", views.signup_page, name="signup"),
  path('profile/<int:user_id>/', views.profile, name='users_profile'),
- path("search/", views.SearchResultsView.as_view(), name="search_results")
+ path("search/", views.SearchResultsView.as_view(), name="search_results"),
+ path("complete-profile/", views.complete_profile_doctor, name="complete_profile")
+
 ]
